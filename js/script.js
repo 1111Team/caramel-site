4c009430e68aa6dc91e72686cb4be4c48f4f57c7
TimBekker <tibekker@gmail.com>
2021-02-04 23:45:03 +0600

    hopend

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
 $(document).ready(() => {
     $('.header_burger').click(function(){
         $('.header_burger, .header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
         });
-    /**$(".header_burger,. ").on("click", "a", function(event) {
-        event.preventDefault();
-        var anchorId  = $(this).attr("href");
-        scrollingDistance = $(anchorId).offset().top - $(".header_burger").height();
-        $("html,body").animate({scrollTop: scrollingDistance}, 1100);**/
 });
     $('.slider').slick({
         arrows: false,
@@ -18,17 +14,17 @@ $(document).ready(() => {
         speed: 300,
         infinite: true,
         autoplay: true,
-        autoplayspeed: 740,
+        autoplayspeed: 1100,
         pauseonfocus: true,
-        pauseonover: true,
+        pauseonover: false,
         pauseondotshover: true,
         centerMode: false,
         variableWidht: false,
     });
 $(document).on('click', function(divclose)
 {
-    if($(divclose.target).closest(".header_burger.active").length == 0 )
-    {
+    if($(divclose.target).closest(".header_burger.active").length == 0 ){
         $('.header_burger.active, .header_menu.active').toggleClass('active');
+        $('body').toggleClass('lock');
     }
 });
